5d90db70dd0acf882457a5bc0f33d1bc4bd1eb7b
Rich Holmes <rs.holmes@gmail.com>
2021-01-05 20:49:08 -0500

    test, ignore

---
+++ x.sh
@@ -0,0 +1,21 @@
+backintime restore .'/Cable tester/cabletester'/ao_tht.pretty '' 1
+backintime restore .'/3340 VCO/Hardware/PCB/3340vco'/ao_tht.pretty '' 1
+backintime restore .'/Octave switch/octave'/ao_tht.pretty '' 1
+backintime restore .'/x.sh'/ao_tht.pretty '' 1
+backintime restore .'/Joystick/joystick'/ao_tht.pretty '' 1
+backintime restore .'/dac_ino/hardware/dac_ino'/ao_tht.pretty '' 1
+backintime restore .'/Protoboard/kosmo_proto_smd'/ao_tht.pretty '' 1
+backintime restore .'/Protoboard/kosmo_proto'/ao_tht.pretty '' 1
+backintime restore .'/Power supply/ww_supply/mfosish_ww_supply'/ao_tht.pretty '' 1
+backintime restore .'/Noise bells/Hardware/PCB/noisebells'/ao_tht.pretty '' 1
+backintime restore .'/Noise bells/Hardware/PCB-aux/noisebells-aux'/ao_tht.pretty '' 1
+backintime restore .'/Bass plus plus/bassplusplus'/ao_tht.pretty '' 1
+backintime restore .'/Megaphone/megamodule'/ao_tht.pretty '' 1
+backintime restore .'/Mikrokosmos-II/mikrokosmos-ii'/ao_tht.pretty '' 1
+backintime restore .'/4072 VCF/4072vcf'/ao_tht.pretty '' 1
+backintime restore .'/Positivizer/Hardware/positivizer'/ao_tht.pretty '' 1
+backintime restore .'/Power breakout/Hardware/PCB/powerbreakout'/ao_tht.pretty '' 1
+backintime restore .'/Mixer/Hardware/PCB/aomixer'/ao_tht.pretty '' 1
+backintime restore .'/MIDI to CV/zzzzz Midi2CV_2_ao/Hardware/PCB/midi2cv_ao'/ao_tht.pretty '' 1
+backintime restore .'/MIDI to CV/Midi2CV_ao/Hardware/PCB/midi2cv_ao'/ao_tht.pretty '' 1
+backintime restore .'/Bus board/busboard10'/ao_tht.pretty '' 1
